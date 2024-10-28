5fa6cdbf64d49c3bb591df202736a8f0c6041caf
AJ Gebara <ajgebara@gmail.com>
2024-10-27 21:56:37 -0500

    made dev enviornment

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+// src/index.tsx
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello, React + TypeScript!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    historyApiFallback: true, // This helps in single page application routing.
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
